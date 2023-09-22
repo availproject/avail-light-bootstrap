d7a7cc273db5dbae5ba2e1b836ab4d7f812de347
momosh <momcilo@ethernal.tech>
2023-09-22 18:45:55 +0200

    using telemetry to record number of peers

--- src/main.rs
+++ src/main.rs
@@ -4,16 +4,20 @@ mod network;
 mod telemetry;
 mod types;
 
+use crate::telemetry::{MetricValue, Metrics};
 use anyhow::{Context, Result};
 use clap::Parser;
 use libp2p::{multiaddr::Protocol, Multiaddr};
-use std::net::Ipv4Addr;
+use std::{net::Ipv4Addr, time::Duration};
+use tokio::time::{interval_at, Instant};
 use tracing::{error, info, metadata::ParseLevelError, warn, Level};
 use tracing_subscriber::{
     fmt::format::{self, DefaultFields, Format, Full, Json},
     FmtSubscriber,
 };
 use types::RuntimeConfig;
+
+const CLIENT_ROLE: &str = "bootstrap_node";
 
 #[derive(Debug, Parser)]
 #[clap(name = "Avail Bootstrap Node")]
@@ -68,13 +72,47 @@ async fn run() -> Result<()> {
         warn!("Using default log level: {err}");
     }
 
-    let (id_keys, _) = network::keypair((&cfg).into())?;
+    let (id_keys, peer_id) = network::keypair((&cfg).into())?;
 
     let (network_client, network_event_loop) = network::init((&cfg).into(), id_keys)
         .context("Failed to initialize P2P Network Service.")?;
 
+    let ot_metrics =
+        telemetry::otlp::initialize(cfg.ot_collector_endpoint, peer_id, CLIENT_ROLE.into())
+            .context("Cannot initialize OpenTelemetry service.")?;
+
     // Spawn the network task
     tokio::spawn(network_event_loop.run());
+
+    // Spawn metrics task
+    let m_network_client = network_client.clone();
+    tokio::spawn(async move {
+        let pause_duration = Duration::from_secs(cfg.metrics_network_dump_interval);
+        let mut interval = interval_at(Instant::now() + pause_duration, pause_duration);
+        // repeat and send commands on given interval
+        loop {
+            interval.tick().await;
+            // try and read current multiaddress
+            if let Ok(multiaddr) = m_network_client.get_multiaddress().await {
+                if let Some(addr) = multiaddr {
+                    // set Multiaddress
+                    _ = ot_metrics.set_multiaddress(addr.to_string());
+                    if let Some(ip) = network::extract_ip(addr) {
+                        // set IP
+                        _ = ot_metrics.set_ip(ip);
+                    }
+                }
+            }
+            if let Ok(counted_peers) = m_network_client.count_dht_entries().await {
+                if let Err(err) = ot_metrics
+                    .record(MetricValue::ActivePeers(counted_peers))
+                    .await
+                {
+                    error!("Error recording network stats metric: {err}");
+                }
+            };
+        }
+    });
 
     // Listen on all interfaces
     network_client
